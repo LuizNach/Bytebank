36509cc1fd05220cfc7df0e8cb86d3ab513efa50
LuizNach <luiz.nach@gmai.com>
2020-02-24 16:50:01 -0300

    Aula4 refactor formulario transferencia

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,15 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text(this.title)),
       body: ListaTransferencias(),
-      floatingActionButton: FloatingActionButton(onPressed: null, child: Icon(Icons.add)),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          final Future<Transferencia> future = Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return FormularioTransferencia();
+          }));
+          future.then( (trasnferencia ){ print(trasnferencia); print('$trasnferencia'); } );
+        }, 
+      child: Icon(Icons.add),
+      ),
     );
   }
 
@@ -81,4 +89,80 @@ class Transferencia {
 
   Transferencia(this.valor,this.conta);
 
+  @override
+  String toString() {
+    return  "Conta criada: <$valor> <$conta>" ;
+  }
+
+}
+
+class FormularioTransferencia extends StatelessWidget {
+
+  final TextEditingController _controladorCampoNumeroConta = TextEditingController();
+  final TextEditingController _controladorCampoValor = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Criando Transferencia")),
+      body: Column(
+        children: <Widget>[
+          Editor(
+            controlador: _controladorCampoNumeroConta,
+            rotulo: "Conta",
+            dica: "0000",
+          ),
+          Editor(
+            controlador: _controladorCampoValor,
+            rotulo: "Valor",
+            dica: "0.00",
+            icone: Icons.monetization_on,
+          ),
+          RaisedButton(
+            child: Text("confirmar"),
+            onPressed: () => _criarTransferencia(context), 
+          )
+        ]
+      )
+    );
+  }
+
+  void _criarTransferencia(BuildContext context){
+    print("clicou confirmar");
+    final int numeroConta = int.tryParse(_controladorCampoNumeroConta.text);
+    final double valor = double.tryParse(_controladorCampoValor.text);
+    if(numeroConta != null && valor != null){
+      final transferenciaCriada = new Transferencia(valor, numeroConta);
+      Navigator.pop(context, transferenciaCriada);
+    }
+  }
+
+}
+
+class Editor extends StatelessWidget {
+
+  final TextEditingController controlador;
+  final String rotulo;
+  final String dica;
+  final IconData icone;
+
+  Editor({this.controlador,this.rotulo,this.dica, this.icone});
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Padding(
+      padding: const EdgeInsets.fromLTRB( 16.0, 8.0, 16.0, 8.0),
+      child: TextField(
+        
+        keyboardType: TextInputType.number,
+        controller: controlador ,
+        style: TextStyle(fontSize: 24.0),
+        decoration: InputDecoration(
+          icon: icone != null ? Icon(icone) : null,
+          labelText: rotulo,
+          hintText: dica,          
+        ),
+      ),);
+  }
 }
