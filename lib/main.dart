dbef29e6c2fb43ba8d320ca4f0a342329b57a8fc
LuizNach <luiz.nach@gmai.com>
2020-02-25 02:35:39 -0300

    Aula5 correcting page routing bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -27,7 +26,6 @@ class MyApp extends StatelessWidget {
 }
 
 class ListaTransferencias extends StatefulWidget {
-
   final String title;
 
   ListaTransferencias({this.title});
@@ -39,7 +37,6 @@ class ListaTransferencias extends StatefulWidget {
 }
 
 class ListaTransferenciasState extends State<ListaTransferencias> {
-
   final List<Transferencia> listaTransferencias = List();
 
   @override
@@ -48,32 +45,36 @@ class ListaTransferenciasState extends State<ListaTransferencias> {
       appBar: AppBar(title: Text(widget.title)),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          final Future<Transferencia> future = Navigator.push(context, MaterialPageRoute(builder: (context) {
+          final Future<Transferencia> future =
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
             return FormularioTransferencia();
           }));
-          future.then( (transferencia ){ 
-            print(transferencia); 
-            print('$transferencia'); 
-            if(transferencia != null)listaTransferencias.add(transferencia);
-            } 
-          );
-        }, 
+          future.then( (transferencia) {
+            Future.delayed( Duration(seconds: 1), () {
+              print(transferencia);
+              print('$transferencia');
+              if (transferencia != null) {
+                setState( () {
+                  listaTransferencias.add(transferencia);
+                } );
+              }
+            } );
+          });
+        },
         child: Icon(Icons.add),
       ),
       body: ListView.builder(
         itemCount: listaTransferencias.length,
-        itemBuilder: (context, index ){
+        itemBuilder: (context, index) {
           final transferencia = listaTransferencias[index];
           return ItemTransferencia(transferencia);
         },
       ),
     );
   }
-
 }
 
 class ItemTransferencia extends StatelessWidget {
-
   final Transferencia transferencia;
 
   ItemTransferencia(this.transferencia);
@@ -85,92 +86,96 @@ class ItemTransferencia extends StatelessWidget {
         title: Text(this.transferencia.valor.toString()),
         subtitle: Text(this.transferencia.conta.toString()),
         leading: Icon(Icons.monetization_on),
-      ),      
+      ),
     );
   }
 }
 
 class Transferencia {
-
   final double valor;
   final int conta;
 
-  Transferencia(this.valor,this.conta);
+  Transferencia(this.valor, this.conta);
 
   @override
   String toString() {
-    return  "Conta criada: <$valor> <$conta>" ;
+    return "Conta criada: <$valor> <$conta>";
   }
-
 }
 
-class FormularioTransferencia extends StatelessWidget {
+class FormularioTransferencia extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return FormularioTransferenciaState();
+  }
+  
+}
 
-  final TextEditingController _controladorCampoNumeroConta = TextEditingController();
+
+class FormularioTransferenciaState extends State<FormularioTransferencia> {
+
+  final TextEditingController _controladorCampoNumeroConta =
+      TextEditingController();
   final TextEditingController _controladorCampoValor = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Criando Transferencia")),
-      body: Column(
-        children: <Widget>[
-          Editor(
-            controlador: _controladorCampoNumeroConta,
-            rotulo: "Conta",
-            dica: "0000",
-          ),
-          Editor(
-            controlador: _controladorCampoValor,
-            rotulo: "Valor",
-            dica: "0.00",
-            icone: Icons.monetization_on,
-          ),
-          RaisedButton(
-            child: Text("confirmar"),
-            onPressed: () => _criarTransferencia(context), 
-          )
-        ]
-      )
-    );
+        appBar: AppBar(title: Text("Criando Transferencia")),
+        body: SingleChildScrollView(
+          child: Column(children: <Widget>[
+            Editor(
+              controlador: _controladorCampoNumeroConta,
+              rotulo: "Conta",
+              dica: "0000",
+            ),
+            Editor(
+              controlador: _controladorCampoValor,
+              rotulo: "Valor",
+              dica: "0.00",
+              icone: Icons.monetization_on,
+            ),
+            RaisedButton(
+              child: Text("confirmar"),
+              onPressed: () => _criarTransferencia(context),
+            )
+          ]),
+        ));
   }
 
-  void _criarTransferencia(BuildContext context){
+  void _criarTransferencia(BuildContext context) {
     print("clicou confirmar");
     final int numeroConta = int.tryParse(_controladorCampoNumeroConta.text);
     final double valor = double.tryParse(_controladorCampoValor.text);
-    if(numeroConta != null && valor != null){
+    if (numeroConta != null && valor != null) {
       final transferenciaCriada = new Transferencia(valor, numeroConta);
       Navigator.pop(context, transferenciaCriada);
     }
   }
-
 }
 
 class Editor extends StatelessWidget {
-
   final TextEditingController controlador;
   final String rotulo;
   final String dica;
   final IconData icone;
 
-  Editor({this.controlador,this.rotulo,this.dica, this.icone});
+  Editor({this.controlador, this.rotulo, this.dica, this.icone});
 
   @override
   Widget build(BuildContext context) {
-    
     return Padding(
-      padding: const EdgeInsets.fromLTRB( 16.0, 8.0, 16.0, 8.0),
+      padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
       child: TextField(
-        
         keyboardType: TextInputType.number,
-        controller: controlador ,
+        controller: controlador,
         style: TextStyle(fontSize: 24.0),
         decoration: InputDecoration(
           icon: icone != null ? Icon(icone) : null,
           labelText: rotulo,
-          hintText: dica,          
+          hintText: dica,
         ),
-      ),);
+      ),
+    );
   }
 }
